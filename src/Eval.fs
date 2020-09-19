b99c901a9e28b8f4d6f200e0bff3b3f3ffa20cc1
davnavr <davidcalieln@gmail.com>
2020-09-19 16:19:42 -0700

    Underlying numeric type is now bigint instead of int64 to allow for a larger range of values

---
+++ src/Eval.fs
@@ -0,0 +1,118 @@
+﻿[<RequireQualifiedAccess>]
+module HexCalc.Eval
+
+open FParsec
+
+let private exprRef = OperatorPrecedenceParser<Integer, unit, unit>()
+
+let private digits ibase (ds: char[]): Parser<_, unit> =
+    let baseval = bigint ds.Length
+    let rec buildint num =
+        function
+        | [] -> num
+        | [] :: tail -> buildint num tail
+        | (digit: int :: tail1) :: tail2 ->
+            let num' = num * baseval + bigint digit
+            let tail' = tail1 :: tail2
+            buildint num' tail'
+    let digit =
+        Array.mapi
+            (fun i d ->
+                string d
+                |> pstringCI
+                >>% i)
+            ds
+        |> choice
+        <?> "digit"
+    let sep =
+        skipChar '_' <?> "separator" |> many1
+    sepBy1
+        (many1 digit)
+        sep
+    |>> (fun digits ->
+        { Base = ibase
+          Value = buildint bigint.Zero digits })
+
+let private integer: Parser<_, unit> =
+    choiceL
+        [
+            let dec = [| '0'..'9' |]
+
+            pstringCI "0b"
+            |> attempt
+            >>. digits Base2 [| '0'; '1' |]
+
+            pstringCI "0x"
+            |> attempt
+            >>. digits Base16 (Array.append dec [| 'a'..'f' |])
+
+            digits Base10 dec
+        ]
+        "integer"
+
+let private ws: Parser<unit, unit> =
+    [
+        skipAnyOf [ '('; ')' ]
+        spaces1
+    ]
+    |> choice
+
+let expr =
+    spaces >>. exprRef.ExpressionParser <?> "expression"
+
+let input =
+    let command names cmd =
+        List.map
+            (skipString >> attempt)
+            names
+        |> choice
+        >>% cmd
+    let help =
+        spaces1 >>. restOfLine false |> opt
+    [
+        expr |>> Input.Eval
+
+        command [ "clear"; "cls" ] Input.Clear
+        command [ "quit"; "exit" ] Input.Quit
+
+        skipString "help"
+        >>. help
+        |>> Input.Help
+    ]
+    |> choice
+    .>> eof
+
+let private mapval map i  =
+    { i with Value = map i.Value }
+let private prefixOp op c prec =
+    PrefixOperator<_, _, _>(c, spaces, prec, true, mapval op) :> Operator<_, _, _>
+let private infixOp op c prec =
+    let op' i1 i2 =
+        { Base = min i1.Base i2.Base
+          Value = op i1.Value i2.Value }
+    InfixOperator<_, _, _>(c, spaces, prec, Associativity.Left, op') :> Operator<_,_,_>
+
+do
+    
+    seq {
+        for op in Terms.operators do
+            let eoper =
+                match op.Operation with
+                | Terms.InfixOp oper -> infixOp oper
+                | Terms.PrefixOp oper -> prefixOp oper
+            eoper op.Symbol op.Precedence
+    }
+    |> Seq.iter exprRef.AddOperator
+
+    exprRef.TermParser <-
+        [
+            integer
+
+            skipChar '('
+            |> attempt
+            >>. expr
+            .>> skipChar ')'
+            <?> "nested expression"
+        ]
+        |> choice
+        .>> spaces
--- src/Types.fs
+++ src/Types.fs
@@ -9,18 +9,12 @@ type Base =
 [<CustomComparison; CustomEquality>]
 type Integer =
     { Base: Base
-      Value: int64 }
+      Value: bigint }
 
     override this.ToString() =
-        let neg = 
-            if this.Value < 0L then " (negative)" else ""
         match this.Base with
         | Base10 -> string this.Value
-        | Base2 ->
-            let bits = System.Convert.ToString(this.Value, 2)
-            sprintf "0b%s%s" bits neg
-        | Base16 ->
-            sprintf "0x%X%s" this.Value neg
+        | _ -> invalidOp "TODO: Add support for binary and hexadecimal, taking to account the weird behavior when the value is negative."
 
     override this.Equals(other) =
         this.Value = (other :?> Integer).Value
@@ -31,29 +25,16 @@ type Integer =
         member this.CompareTo(other) =
             this.Value.CompareTo (other :?> Integer).Value
 
-[<StructuralComparison; StructuralEquality>]
-type Expression =
-    | Integer of Integer
-    | Add of Expression * Expression
-    | Sub of Expression * Expression
-    | Mul of Expression * Expression
-    | Div of Expression * Expression
-    | Modulo of Expression * Expression
-    | Negate of Expression
-    | And of Expression * Expression
-    | Or of Expression * Expression
-    | Xor of Expression * Expression
-
 [<RequireQualifiedAccess>]
 type Input =
-    | Expr of Expression
+    | Eval of result: Integer
     | Help of term: string option
     | Clear
     | Quit
 
 [<RequireQualifiedAccess>]
 type Output =
-    | Result of Integer
+    | Result of string
     | Error of msg: string
     | Messages of lines: seq<string>
     | Clear
